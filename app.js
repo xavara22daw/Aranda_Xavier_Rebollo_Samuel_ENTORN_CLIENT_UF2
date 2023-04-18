c07f2c902c9d9b7cdc11b1d8becd587f4a8cd983
samreb22daw <samreb22daw@proton.me>
2023-04-18 13:00:31 +0200

    Actualizando el proyecto

--- app.js
+++ app.js
@@ -38,41 +38,41 @@ crearTablero("pink", "ordenador");
 
 //Crear barcos
 class barco {
-    constructor(nombre, celdas) {
-      this.nombre = nombre;
-      this.celdas = celdas;
-    }
+  constructor(nombre, celdas) {
+    this.nombre = nombre;
+    this.celdas = celdas;
   }
-  
-  class submarine extends barco {
-    constructor() {
-      super("submarine", 1);
-    }
+}
+
+class submarine extends barco {
+  constructor() {
+    super("submarine", 1);
   }
-  
-  class destroyer extends barco {
-      constructor() {
-          super("destroyer", 2);
-      }
+}
+
+class destroyer extends barco {
+  constructor() {
+    super("destroyer", 2);
   }
-  
-  class cruiser extends barco {
-      constructor() {
-          super("cruiser", 3);
-      }
+}
+
+class cruiser extends barco {
+  constructor() {
+    super("cruiser", 3);
   }
-  
-  class battleship extends barco {
-      constructor() {
-          super("battleship", 4);
-      }
+}
+
+class battleship extends barco {
+  constructor() {
+    super("battleship", 4);
   }
-  
-  class carrier extends barco {
-      constructor() {
-          super("carrier", 5);
-      }
+}
+
+class carrier extends barco {
+  constructor() {
+    super("carrier", 5);
   }
+}
 
 const submarine1 = new submarine();
 const submarine2 = new submarine();
@@ -92,3 +92,31 @@ const barcos = [
   carrier1,
 ];
 console.log(barcos);
+
+const addBarcosOrdenador = (barco) => {
+  // Se puede probar a hacer Set
+  const celdasTablero = document.querySelectorAll("#ordenador div");
+  let randomBoolean = Math.random() < 0.5;
+  let isHorizontal = randomBoolean;
+  let randomIndiceInicial = Math.floor(Math.random() * width * width);
+  console.log(randomIndiceInicial);
+
+  // Se puede probar a hacer Set
+  bloquesBarcos = [];
+
+  for (let i = 0; i < barco.celdas; i++) {
+    if (isHorizontal) {
+      bloquesBarcos.push(celdasTablero[Number(randomIndiceInicial) + i]);
+    }else {
+      bloquesBarcos.push(celdasTablero[Number(randomIndiceInicial) + i * width]);
+    }
+  }
+
+  bloquesBarcos.forEach(bloqueBarco => {
+    bloqueBarco.classList.add(barco.nombre);
+    bloqueBarco.classList.add('taken');
+  })
+};
+
+addBarcosOrdenador(destroyer1);
+
